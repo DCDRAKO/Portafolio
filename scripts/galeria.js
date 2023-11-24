a94b1d14dcf10e0b6b8dd761a8ff6442589ddceb
DESKTOP-2VV5BMN\Daniel <est.daniel.pedroza@unimilitar.edu.co>
2023-11-23 23:21:02 -0500

    Ingreso del Codigo de la Galeria

---
+++ scripts/galeria.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtén todos los elementos con la clase "lista__item"
+    var imagenes = document.querySelectorAll(".lista__item");
+
+    // Obtén el div "muestra"
+    var muestra = document.querySelector(".muestra__imagen");
+    var muestraIframe=document.querySelector(".muestra__iframe");
+
+    // Agrega un evento de escucha a cada elemento con la clase "lista__item"
+    imagenes.forEach(function (imagen) {
+        imagen.addEventListener("click", function () {
+            // Obtén la URL de la imagen o iframe dentro del elemento actual
+            var mediaElement = imagen.querySelector("img, iframe");
+            
+            if (mediaElement) {
+                var mediaUrl = mediaElement.src;
+
+                // Cambia la imagen o iframe en el div "muestra" con el elemento actual
+                if (mediaElement.tagName === 'IMG') {
+                    muestra.src = mediaUrl;
+                    muestraIframe.src = "";
+                } else if (mediaElement.tagName === 'IFRAME') {
+                    muestraIframe.src = mediaUrl;
+                   
+                    muestra.src="";
+                }
+            }
+        });
+    });
+});
